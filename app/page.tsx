34c2b0bb2f1e19a2a290c48dc6622b0abc2de6de
Rafael Tavares <rafatavares03@hotmail.com>
2026-06-04 18:48:21 -0300

    banner da home ajustado

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="h-screen items-center justify-center bg-[url('/banner-bg.jpg')] bg-cover bg-center font-sans dark:bg-black">
-      <div className="flex flex-col items-center justify-center bg-black/50 w-full h-full">
-        <div className="flex flex-col text-amber-50 w-7xl">
-          <h1 className="text-5xl">Bem vindo ao <span className="text-amber-300">Libris</span>!</h1>
-          <p className="text-lg my-5">Registre suas leituras, avalie seus livros favoritos e interaja com outros leitores.</p>
+      <div className="flex flex-col items-begin justify-center bg-black/50 w-full h-full">
+        <div className="h-full w-sm flex flex-col items-center justify-around bg-amber-50 text-black text-center">
+            <Image src="/libris-lg.png" width={100} height={100} alt="Libris logo"/>
+            <div className="p-2">
+
+              <h1 className="text-5xl">Bem-vindo!</h1>
+              <p className="my-5">Registre suas leituras, avalie seus livros favoritos e interaja com outros leitores.</p>
+
+            </div>
+            <p>Acesse sua conta <a className="text-yellow-500" href="http://">agora</a> ou <a className="text-yellow-500" href="http://">cadastre-se</a></p>
         </div>
       </div>
     </div>
   );
+
 }
