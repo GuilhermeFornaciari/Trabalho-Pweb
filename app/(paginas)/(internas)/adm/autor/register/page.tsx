c70e852a5627de9f0da5738c25d53eff2b68bc47
Rafael Tavares <rafatavares03@hotmail.com>
2026-06-08 23:26:56 -0300

    correcao de imports

--- app/(paginas)/(internas)/adm/autor/register/page.tsx
+++ app/(paginas)/(internas)/adm/autor/register/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import Header from "@/components/header";
 
 export default function RegisterAuthor() {
   const [autor, setAutor] = useState("");
--- app/(paginas)/(internas)/adm/colecao/register/page.tsx
+++ app/(paginas)/(internas)/adm/colecao/register/page.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import Header from "@/components/header";
 import { Livro } from "@/lib/prisma/generated/client";
 
 export default function RegisterColection() {
