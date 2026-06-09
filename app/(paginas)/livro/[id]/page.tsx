92637dbe16dce97520207d93bfead3cad3e9895f
hugostoso1 <hugoaasouza45@gmail.com>
2026-06-08 23:41:40 -0300

    modal de exluir bao

--- app/(paginas)/livro/[id]/page.tsx
+++ app/(paginas)/livro/[id]/page.tsx
@@ -2,8 +2,9 @@
 
 import { use, useEffect, useState } from "react";
 import { Livro } from "@/lib/prisma/generated/client";
-import Header from "@/components/header";
+import { useRouter } from 'next/navigation'
 import Link from "next/link";
+import { Router } from "next/router";
 
 type LivroDetalhes = Livro & {
   autores: Array<{id: number, nome: string}> 
@@ -18,6 +19,10 @@ export default function DetalhesLivro({
   const [livro, setLivro] = useState<LivroDetalhes | null>(null);
   const buttonStyle = "px-5 py-3 rounded-md"
 
+  const [mostrarModal, setMostrarModal] = useState(false);
+
+  const route = useRouter();
+
   useEffect(() => {
     const carregarLivro = async () => {
       const response = await fetch(`/api/livro/${id}`);
@@ -28,17 +33,64 @@ export default function DetalhesLivro({
     }
     carregarLivro();
   }, [])
-
   
+  async function apagarLivro(id: number, colecaoId: number | null) {
+
+    const res = await fetch(`/api/livro/delete?id=${id}&colecaoId=${colecaoId}`, {
+      method: "DELETE",
+    });
+
+    if (!res.ok) {
+      alert("Erro ao apagar livro");
+      return;
+    }
+    
+    route.push("/catalogo");
+    // alert("Livro apagado com sucesso");
+  }
+
   return(
       (livro) ? 
         <>
           {informacoesDoLivro(livro)}
           <div className="w-2xl m-auto flex justify-around">
             <Link href={`/adm/livro/edit/${id}`} className={buttonStyle + " bg-yellow-400"}>Editar</Link>
-            <button className={buttonStyle + " bg-red-500"}>Apagar</button>
+            <button className={buttonStyle + " bg-red-500"} type="button" onClick={() => setMostrarModal(true)} >Apagar</button>
           </div>
-        </>
+
+
+          {mostrarModal && (
+            <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
+              <div className="bg-white p-6 rounded-md shadow-lg min-w-sm">
+                <h2 className="text-lg font-semibold mb-4">
+                  Confirmar exclusão
+                </h2>
+
+                <p className="mb-6">
+                  Tem certeza que deseja apagar este livro?
+                </p>
+
+                <div className="flex justify-end gap-3">
+                  <button
+                    type="button"
+                    className="px-4 py-2 bg-gray-300 rounded-md"
+                    onClick={() => setMostrarModal(false)}
+                  >
+                    Não
+                  </button>
+
+                  <button
+                    type="button"
+                    className="px-4 py-2 bg-red-500 text-white rounded-md"
+                    onClick={() => { apagarLivro(livro.id, livro.colecaoId) }}
+                  >
+                    Sim
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </>       
       : ''
   );
 }
